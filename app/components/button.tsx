0c7f23dc88b1c397a6e40709e03c4bb237367ab1
Taek Been Nam <taekbeen93@gmail.com>
2023-11-08 23:44:23 -0500

    feature: add sound effect

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -9,27 +9,35 @@ import { Heart } from "react-feather";
 function Button() {
   const [count, setCount] = useState(0); //temporary clientside state
   const [isCountUp, setIsCountUp] = useState(true);
+  const audio = new Audio("./shooting-sound.mp3");
+  const audioTwo = new Audio("./pick.m4a");
 
   const handleIncrement = () => {
     setCount((count) => count + 1);
     setIsCountUp(true);
+    audio.preservesPitch = false;
+    audio.playbackRate = (count + 1) / 3; // divide by 3 to start from the lower pitch
+    audio.play();
+    count === 9 && audioTwo.play();
   };
+
   const handleDecrement = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
     e.preventDefault();
-
     setCount((count) => count - 1);
     setIsCountUp(false);
+    audio.preservesPitch = false;
+    audio.playbackRate = (count + 1) / 3;
+    audio.play();
   };
 
-  console.log(count, isCountUp);
   return (
     <div className="flex w-60 gap-6 p-8">
       <button
-        className="relative h-16 w-16 origin-center rounded-xl antialiased transition-transform duration-300 hover:scale-[1.3] focus-visible:scale-[1.3] active:scale-[1.1]"
+        className="relative h-16 w-16 origin-center rounded-xl transition-transform duration-300 hover:scale-[1.2] focus-visible:scale-[1.2] active:scale-[1.4]"
         type="button"
-        onClick={() => count < 5 && handleIncrement()}
+        onClick={() => count < 10 && handleIncrement()}
         onContextMenu={(e) => count > 0 && handleDecrement(e)}
       >
         <LikeIcon likes={count} />
@@ -37,7 +45,7 @@ function Button() {
       <div className="relative self-end">
         <span
           key={count}
-          className="animate-counter absolute -translate-y-8 text-zinc-600 opacity-0"
+          className="absolute -translate-y-8 animate-counter text-zinc-600 opacity-0"
         >
           {isCountUp ? "+1" : "-1"}
         </span>
--- app/components/likeIcon.tsx
+++ app/components/likeIcon.tsx
@@ -14,33 +14,56 @@ const WAVE_PATH_THREE =
   "M0 529L18.8 535C37.7 541 75.3 553 112.8 560.5C150.3 568 187.7 571 225.2 565.7C262.7 560.3 300.3 546.7 337.8 535.8C375.3 525 412.7 517 450.2 517.7C487.7 518.3 525.3 527.7 562.8 535C600.3 542.3 637.7 547.7 675.2 554.5C712.7 561.3 750.3 569.7 787.8 560.3C825.3 551 862.7 524 881.3 510.5L900 497L900 601L881.3 601C862.7 601 825.3 601 787.8 601C750.3 601 712.7 601 675.2 601C637.7 601 600.3 601 562.8 601C525.3 601 487.7 601 450.2 601C412.7 601 375.3 601 337.8 601C300.3 601 262.7 601 225.2 601C187.7 601 150.3 601 112.8 601C75.3 601 37.7 601 18.8 601L0 601Z";
 
 function LikeIcon({ likes }: LikeIconProps) {
+  const translateYVariants: { [key in string]: string } = {
+    "0": "translate-y-[20px]",
+    "1": "translate-y-[16px]",
+    "2": "translate-y-[14px]",
+    "3": "translate-y-[12px]",
+    "4": "translate-y-[10px]",
+    "5": "translate-y-[8px]",
+    "6": "translate-y-[6px]",
+    "7": "translate-y-[4px]",
+    "8": "translate-y-[2px]",
+    "9": "translate-y-[0px]",
+    "10": "-translate-y-[2px]",
+  };
   return (
     <svg viewBox="0 0 24 24">
       <defs>
-        <clipPath id="mask" mask-type="alpha" maskUnits="userSpaceOnUse">
+        <clipPath id="mask">
           <path d={HEART_PATH} />
         </clipPath>
       </defs>
-      <g clipPath="url(#mask)" overflow="hidden">
+      <g clipPath="url(#mask)">
         <g
-          className={`transition-transform duration-500 ease-out
-          ${likes === 0 ? "translate-y-3" : ""}
-          ${likes === 1 ? "translate-y-1" : ""}
-          ${likes === 2 ? "translate-y-0" : ""}
-          ${likes === 3 ? "-translate-y-1" : ""}
-          ${likes === 4 ? "-translate-y-2" : ""}
-          ${likes === 5 ? "-translate-y-3" : ""}`}
+          className={`transition-transform duration-500 ease-out 
+          ${translateYVariants[String(likes)]}
+          `}
         >
           <svg viewBox="0 0 900 900" overflow="hidden">
-            <path id="wave-1" d={WAVE_PATH_ONE} fill="#fa7268"></path>
-            <path id="wave-2" d={WAVE_PATH_TWO} fill="#e34c67"></path>
-            <path id="wave-3" d={WAVE_PATH_THREE} fill="#c62368"></path>
+            <path
+              id="wave-1"
+              d={WAVE_PATH_ONE}
+              fill="#fa7268"
+              transform="translate(0,-650)"
+            ></path>
+            <path
+              id="wave-2"
+              d={WAVE_PATH_TWO}
+              fill="#e34c67"
+              transform="translate(0,-650)"
+            ></path>
+            <path
+              id="wave-3"
+              d={WAVE_PATH_THREE}
+              fill="#c62368"
+              transform="translate(0,-650)"
+            ></path>
+            <rect
+              className={`h-full w-full fill-current text-[#c62368]`}
+              transform="translate(0,200)"
+            />
           </svg>
-          <rect
-            y={16}
-            className={`h-full w-full fill-current text-[#c62368]
-          `}
-          />
         </g>
       </g>
       <path
