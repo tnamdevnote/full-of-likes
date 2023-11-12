62311549f0d53362f7d3a887453e2d33b5139949
Taek Been Nam <taekbeen93@gmail.com>
2023-11-11 20:45:40 -0500

    fix: resolve audio reference error

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -9,16 +9,20 @@ import { Heart } from "react-feather";
 function Button() {
   const [count, setCount] = useState(0); //temporary clientside state
   const [isCountUp, setIsCountUp] = useState(true);
-  const audio = new Audio("./shooting-sound.mp3");
-  const audioTwo = new Audio("./pick.m4a");
+  const [audio, setAudio] = useState<HTMLAudioElement>();
+  const [audioTwo, setAudioTwo] = useState<HTMLAudioElement>();
+  // const audio = new Audio("./shooting-sound.mp3");
+  // const audioTwo = new Audio("./pick.m4a");
 
   const handleIncrement = () => {
     setCount((count) => count + 1);
     setIsCountUp(true);
-    audio.preservesPitch = false;
-    audio.playbackRate = (count + 1) / 3; // divide by 3 to start from the lower pitch
-    audio.play();
-    count === 9 && audioTwo.play();
+    if (audio && audioTwo) {
+      audio.preservesPitch = false;
+      audio.playbackRate = (count + 1) / 3; // divide by 3 to start from the lower pitch
+      audio.play();
+      count === 9 && audioTwo.play();
+    }
   };
 
   const handleDecrement = (
@@ -27,10 +31,17 @@ function Button() {
     e.preventDefault();
     setCount((count) => count - 1);
     setIsCountUp(false);
-    audio.preservesPitch = false;
-    audio.playbackRate = (count + 1) / 3;
-    audio.play();
+    if (audio) {
+      audio.preservesPitch = false;
+      audio.playbackRate = (count + 1) / 3;
+      audio.play();
+    }
   };
+
+  useEffect(() => {
+    setAudio(new Audio("./shooting-sound.mp3"));
+    setAudioTwo(new Audio("./pick.m4a"));
+  }, []);
 
   return (
     <div className="flex w-60 gap-6 p-8">
@@ -47,7 +58,7 @@ function Button() {
           key={count}
           className="absolute -translate-y-8 animate-counter text-zinc-600 opacity-0"
         >
-          {isCountUp ? "+1" : "-1"}
+          {count > 0 && (isCountUp ? "+1" : "-1")}
         </span>
         <span className="text-xl text-zinc-600">{count}</span>
       </div>
