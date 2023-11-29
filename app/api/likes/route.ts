ad30eee1d9f2f2897318730d3a306604f9902741
Taek Been Nam <taekbeen93@gmail.com>
2023-11-29 00:49:17 -0500

    fix: PUT -> PATCH

--- app/api/likes/route.ts
+++ app/api/likes/route.ts
@@ -56,7 +56,7 @@ export async function POST(request: Request) {
   }
 }
 
-export async function PUT(request: Request) {
+export async function PATCH(request: Request) {
   try {
     const currentUserId = getCurrentUserId(request.headers);
     await prisma.like.update({
